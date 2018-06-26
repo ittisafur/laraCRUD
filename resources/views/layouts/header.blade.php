50bae3bbd76346577d1265fef3441f219b4dd14f
ittisafur <ittisafur@gmail.com>
2018-06-26 18:20:39 +0600

    CRUD v2

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,9 +3,6 @@
     <nav class="blog-nav">
       <a class="blog-nav-item active" href="/">Home</a>
       <a class="blog-nav-item" href="/create">Create</a>
-      <a class="blog-nav-item" href="#">Press</a>
-      <a class="blog-nav-item" href="#">New hires</a>
-      <a class="blog-nav-item" href="#">About</a>
     </nav>
   </div>
 </div>
